df82738414127ea913cff9cbace771e0ce4a4a5d
Alon Kwart <alon.kwart@gmail.com>
2018-04-29 02:37:06 +0300

    main - need to fix load and impute
    Signed-off-by: Alon Kwart <alon.kwart@gmail.com>

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ class Stages:
     # Stages:
     do_print = True
     do_get_raw_data = False
-    do_load_and_impute = False
-    do_scale = False
+    do_load_and_impute = True
+    do_scale = True
     do_feature_selection = True
     do_removeAbove95Corr = False
     do_sfs = True
@@ -45,8 +45,8 @@ def main():
 
         for i in range(1, amount_of_sets + 1):
             # start the preparing data class
-            secondStepPrep_dict[i] = EDP(RAW_SPLIT_FILE_PATH.format(i, "X_train", "{}No_Nan".format(i)),
-                                         RAW_SPLIT_FILE_PATH.format(i, "X_val", "{}Numeric".format(i)),
+            secondStepPrep_dict[i] = EDP(RAW_SPLIT_FILE_PATH.format(i, "X_train", i),
+                                         RAW_SPLIT_FILE_PATH.format(i, "X_val", i),
                                          RAW_SPLIT_FILE_PATH.format(i, "X_test", i),
                                          RAW_SPLIT_FILE_PATH.format(i, "Y_train", i),
                                          RAW_SPLIT_FILE_PATH.format(i, "Y_val", i),
@@ -55,10 +55,10 @@ def main():
             # Swap strings to numeric values
             # Impute missing data
             # Impute outlier and typos
-            # secondStepPrep_dict[i].loadAndImpute(Consts.listAdditionalDataPreparation)
-            secondStepPrep_dict[i].loadData(Consts.listAdditionalDataPreparation)
-            secondStepPrep_dict[i]._dataImpute(secondStepPrep_dict[i].trainData, secondStepPrep_dict[i].valData,
-                                               RAW_SPLIT_FILE_PATH.format(1, "X_val", "1"))
+            secondStepPrep_dict[i].loadAndImpute(Consts.listAdditionalDataPreparation)
+            # secondStepPrep_dict[i].loadData(Consts.listAdditionalDataPreparation)
+            # secondStepPrep_dict[i]._dataImpute(secondStepPrep_dict[i].trainData, secondStepPrep_dict[i].valData,
+            #                                   RAW_SPLIT_FILE_PATH.format(1, "X_val", "1"))
 
     if Stages.do_scale:
         if Stages.do_print:
@@ -114,7 +114,7 @@ def main():
 
             if Stages.do_relief:
                 relief_dir = 'datasets\\{}\\'.format(i)
-                N = 1000
+                N = 100
                 tau = 0
 
                 relief_chosen_set = relief.relief_alg(secondStepPrep_dict[i].trainData,
@@ -128,8 +128,13 @@ def main():
             if Stages.do_removeAbove95Corr:
                 secondStepPrep_dict[i].removeAbove95Corr()
 
-            # create a random forest for the sfs
+            trainData = secondStepPrep_dict[i].trainData.copy()
+            valData = secondStepPrep_dict[i].valData.copy()
+            testData = secondStepPrep_dict[i].testData.copy()
+
+
             if Stages.do_sfs:
+                # create a random forest for the sfs
                 rClf = RandomForestClassifier()
                 max_amount_of_features = 23
                 bestFeatures = sfsAux(rClf, secondStepPrep_dict[i].trainData, secondStepPrep_dict[i].trainLabels,
@@ -138,10 +143,25 @@ def main():
                 secondStepPrep_dict[i].trainData = secondStepPrep_dict[i].trainData[bestFeatures]
                 secondStepPrep_dict[i].valData = secondStepPrep_dict[i].valData[bestFeatures]
                 secondStepPrep_dict[i].testData = secondStepPrep_dict[i].testData[bestFeatures]
-                pd.DataFrame(bestFeatures).to_csv(RAW_SPLIT_FILE_PATH.format(i,"Best_chosen_features",i))
-                secondStepPrep_dict[i].trainData.to_csv(RAW_SPLIT_FILE_PATH.format(i,"Best_chosen_train",i))
-                secondStepPrep_dict[i].valData.to_csv(RAW_SPLIT_FILE_PATH.format(i,"Best_val_chosen",i))
-                secondStepPrep_dict[i].testData.to_csv(RAW_SPLIT_FILE_PATH.format(i,"Best_test_chosen",i))
+                pd.DataFrame(bestFeatures).to_csv(RAW_SPLIT_FILE_PATH.format(i,"Best_chosen_features_random_forest",i))
+                secondStepPrep_dict[i].trainData.to_csv(RAW_SPLIT_FILE_PATH.format(i,"Best_chosen_train_random_forest",i))
+                secondStepPrep_dict[i].valData.to_csv(RAW_SPLIT_FILE_PATH.format(i,"Best_val_chosen_random_forest",i))
+                secondStepPrep_dict[i].testData.to_csv(RAW_SPLIT_FILE_PATH.format(i,"Best_test_chosen_random_forest",i))
+
+                # create svm for the sfs
+
+                svm = SVC()
+                bestFeatures = sfsAux(svm, trainData, secondStepPrep_dict[i].trainLabels,
+                                      max_amount_of_features)
+                print("Sfs chose: {}".format(",".join(bestFeatures)))
+                trainData = trainData[bestFeatures]
+                valData = valData[bestFeatures]
+                testData = testData[bestFeatures]
+                pd.DataFrame(bestFeatures).to_csv(RAW_SPLIT_FILE_PATH.format(i,"Best_chosen_features_svm",i))
+                trainData.to_csv(RAW_SPLIT_FILE_PATH.format(i,"Best_chosen_features_svm",i))
+                valData.to_csv(RAW_SPLIT_FILE_PATH.format(i,"Best_chosen_features_svm",i))
+                testData.to_csv(RAW_SPLIT_FILE_PATH.format(i,"Best_chosen_features_svm",i))
+
 
 
 
